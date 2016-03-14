75b35886b6b0a7a6f0f6c690db5c87af21a58b4f
Alex Cheung <itisalex@163.com>
2016-03-14 21:42:44 +0800

    UILabel, UIImageView, UIButton

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# iOS_tutorial
+## 写给设计师的iOS前端教程
+
+1. [前言](http://www.jianshu.com/p/329169bbf523)
+2. [UILabel](http://www.jianshu.com/p/268eaf92a2a2)
+3. [UIImageView](http://www.jianshu.com/p/7cc16710f519)
--- iOS_tutorial/ViewController.m
+++ iOS_tutorial/ViewController.m
@@ -17,6 +17,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"我是标题";
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    if ([self respondsToSelector:@selector(edgesForExtendedLayout)]) {
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+    }
+    
+    UILabel *myLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, self.view.frame.size.width - 40, 120)];
+    myLabel.text = @"这是多行的文字。这是多行的文字。这是多行的文字。这是多行的文字。这是多行的文字。这是多行的文字。这是多行的文字。这是多行的文字。";
+    myLabel.textColor = [UIColor blueColor];
+    myLabel.textAlignment = NSTextAlignmentCenter;
+    myLabel.font = [UIFont systemFontOfSize:17];
+    myLabel.numberOfLines = 2;
+    myLabel.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:myLabel];
+    
+    UIImageView *myImageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 160, 200, 200)];
+    myImageView.image = [UIImage imageNamed:@"test_image"];
+    myImageView.contentMode = UIViewContentModeScaleAspectFill;
+    myImageView.layer.cornerRadius = 6;
+    myImageView.layer.masksToBounds = YES;
+    [self.view addSubview:myImageView];
+    
+    UIButton *myButton = [[UIButton alloc] initWithFrame:CGRectMake(20, 380, 160, 44)];
+    [myButton setTitle:@"按钮的文字" forState:UIControlStateNormal];
+    [myButton.titleLabel setFont:[UIFont systemFontOfSize:17]];
+    [myButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [myButton setTitleColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
+    //[myButton setBackgroundColor:[UIColor blueColor]];
+    [myButton setBackgroundImage:[UIImage imageNamed:@"button_blue"] forState:UIControlStateNormal];
+    [myButton setBackgroundImage:[UIImage imageNamed:@"button_orange"] forState:UIControlStateHighlighted];
+    [myButton.layer setMasksToBounds:YES];
+    [myButton.layer setCornerRadius:6];
+    [self.view addSubview:myButton];
 }
 
 - (void)didReceiveMemoryWarning {

ad678b1c3ae4aacd8a1ec7a0772b94e7403e10c1
Alex Cheung <itisalex@163.com>
2016-03-14 21:32:56 +0800

    init

---
+++ iOS_tutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_tutorial
+//
+//  Created by 张智超 on 16/3/9.
+//  Copyright © 2016年 张智超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

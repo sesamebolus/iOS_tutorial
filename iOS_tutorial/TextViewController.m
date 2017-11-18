780c6a3cc21174c0ce1751ef8f0f86cdbd17c461
Alex Cheung <itisalex@163.com>
2017-11-18 22:36:20 +0800

    行间距

--- iOS_tutorial/TextViewController.m
+++ iOS_tutorial/TextViewController.m
@@ -38,6 +38,16 @@
     myTextView.font = [UIFont systemFontOfSize:16];
     myTextView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.1];
     [self.view addSubview:myTextView];
+    
+//    UITextView *myTextView = [[UITextView alloc] initWithFrame:CGRectMake(20, 152, self.view.frame.size.width - 40, 220)];
+//    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+//    paragraphStyle.lineSpacing = 10;
+//    NSDictionary *attributes = @{NSForegroundColorAttributeName:[UIColor darkTextColor],
+//                                 NSFontAttributeName:[UIFont systemFontOfSize:16],
+//                                 NSParagraphStyleAttributeName: paragraphStyle
+//                                 };
+//    myTextView.attributedText = [[NSAttributedString alloc] initWithString:@"大江东去，浪淘尽，千古风流人物。故垒西边，人道是，三国周郎赤壁。乱石穿空，惊涛拍岸，卷起千堆雪。江山如画，一时多少豪杰。遥想公瑾当年，小乔初嫁了，雄姿英发。羽扇纶巾，谈笑间，樯橹灰飞烟灭。故国神游，多情应笑我，早生华发。人生如梦，一樽还酹江月。" attributes:attributes];
+//    [self.view addSubview:myTextView];
 }
 
 - (void)didReceiveMemoryWarning {
